101cef541f993ee0a1e8a8c5142fff096b043da3
Cymetria <cymetria@MacBook-Pro-de-Cymetria-1.local>
2016-09-26 20:42:00 -0500

    Primer commit del proyecto: Poniendo en marcha el proceso de automatizaciòn de CSS y JS

---
+++ Gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var cleanCSS = require('gulp-clean-css');
+var merge = require('merge-stream');
+
+gulp.task('less', function(){
+	return gulp.src(['./less/**/*.less', '!less/vars.less', '!less/main.less'])
+			.pipe(less())
+			.pipe(cleanCSS())
+			.pipe(gulp.dest('./css/'));
+});
+
+gulp.task('bootstrap', function(){
+	var bootstrapCSS = gulp.src('./node_modules/bootstrap/dist/css/bootstrap.min.css')
+			.pipe(gulp.dest('./css'));
+
+	var bootstrapFonts = gulp.src('./node_modules/bootstrap/dist/fonts/**/*.*')
+			.pipe(gulp.dest('./fonts'));
+
+	var bootstrapJS = gulp.src('./node_modules/bootstrap/dist/js/bootstrap.min.js')
+			.pipe(gulp.dest('./js'));
+
+	return merge(bootstrapCSS, bootstrapJS, bootstrapFonts);
+});
+
+gulp.task('jquery', function(){
+	return gulp.src('./node_modules/jquery/dist/jquery.min.js')
+			.pipe(gulp.dest('./js'));
+});
+
+gulp.task('default', ['less', 'bootstrap', 'jquery']);
+
+
+
+
+
+
+
