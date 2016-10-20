6c4a3dc272221ff70ed0db2a9031c475f429a60e
Gilson Rincón <gilson.rincon@gmail.com>
2016-10-20 16:23:30 -0500

    Rutas, vistas y un poco mas

--- angular-app/js/app.js
+++ angular-app/js/app.js
@@ -1,1 +1,18 @@
-var app = angular.module('shop', []);
+var app = angular.module('shop', ['ngRoute']);
+
+angular.
+  module('shop').
+  config(['$locationProvider', '$routeProvider',
+    function config($locationProvider, $routeProvider) {
+      $locationProvider.hashPrefix('!');
+
+      $routeProvider.
+        when('/', {
+          template: '<product-list class="row"></product-list>'
+        }).
+        when('/product/:product_id', {
+          template: '<product class="row"></product>'
+        }).
+        otherwise('/');
+    }
+  ]);
