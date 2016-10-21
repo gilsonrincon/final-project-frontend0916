75c083ea439f76db439d9c6d6e1b4dc59d35125b
Cymetria <cymetria@MacBook-Pro-de-Cymetria-1.local>
2016-10-20 20:23:24 -0500

    Añadimos rutas

--- angular/js/app.js
+++ angular/js/app.js
@@ -1,1 +1,13 @@
-var app = angular.module('shop', []);
+var app = angular.module('shop', ['ngRoute']);
+
+angular.module('shop')
+      .config(["$locationProvider", "$routeProvider",
+        function conf($locationProvider, $routeProvider){
+          $locationProvider.hashPrefix('!');
+
+          $routeProvider.when('/',{
+            templateUrl: "Hola"
+          }).when('/product', {
+            template: "Producto"
+          }).otherwise('/');
+        }]);
