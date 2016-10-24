676a04624d7b037bb1461142d09513c9be894604
Gilson Rincón <gilson.rincon@gmail.com>
2016-10-24 05:28:28 -0500

    New layout para el index añadido

--- angular/js/app.js
+++ angular/js/app.js
@@ -5,14 +5,9 @@ angular.module('shop')
         function conf($locationProvider, $routeProvider){
           $locationProvider.hashPrefix('!');
 
-          var index_html = '<nav-menu></nav-menu>';
-          index_html +='<div class="container">';
-          index_html +='<div class="row"><banner></banner></div>';
-          index_html +='<div class="row"><product-list></product-list></div>';
-          index_html +='</div>';
 
           $routeProvider.when('/',{
-            template: index_html
+            templateUrl: 'views/layout-index.html'
           }).when('/product', {
             template: "Producto"
           }).otherwise('/');
