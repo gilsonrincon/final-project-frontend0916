8290304b0f83e52085e515c04daef64868f91d1d
Gilson Rincón <gilson.rincon@gmail.com>
2016-10-24 06:35:06 -0500

    Posibilidades de ordenar productos

--- angular/js/product-list-component.js
+++ angular/js/product-list-component.js
@@ -2,6 +2,8 @@ app.component('productList', {
 	templateUrl: "views/product-list-tpl.html",
 	controller: function ProductListController($http){
 		var self = this;
+
+		self.orderProp = "name";
 		$http.get('product-list.json').then(function(response){
 			self.products = response.data;
 		});
